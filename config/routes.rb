aef2f0f73f39c7d9cc4f14ad666680f1fd9b8be7
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-05-28 10:55:53 +0000

    途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :books
   get '/' => 'homes#top'
-  get 'edit_book' => book_path(@book.id)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

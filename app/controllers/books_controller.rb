a6396930858c690a8ad81d962602044955aff50b
Kasumi Okuda <ec2-user@ip-172-31-28-235.ap-northeast-1.compute.internal>
2023-05-29 14:31:39 +0000

    show途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.save
-    redirect_to '/'
+     redirect_to '/'
   end
 
   def index
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.find_by(id:params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :books
   get '/' => 'homes#top'
+  get 'books/new'
+  post 'books' => 'books#create'  
+  get 'books/:id' => 'books#show', as: 'book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
